75780db45469f13c182cfce52a9d1241d4e53a6b
shekar345 <mallikchary2775@gmail.com>
2025-04-03 11:13:31 +0530

    commit

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { appContext } from "../App";
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import Orders from "./Orders";
 export default function Cart() {
-  const { cart, setCart, products } = useContext(appContext);
+  const { cart, setCart, products, user, orders, setOrders } =
+    useContext(appContext);
+  const [orderValue, setOrderValue] = useState(0);
+  const Navigate = useNavigate();
   const handleDelete = (id) => {
     setCart({ ...cart, [id]: 0 });
   };
@@ -12,8 +17,33 @@ export default function Cart() {
   const decrement = (id) => {
     setCart({ ...cart, [id]: cart[id] - 1 });
   };
+  const placeOrder = () => {
+    setOrders([
+      ...orders,
+      {
+        orderDate: Date(),
+        email: user.email,
+        items: cart,
+        total: orderValue,
+        status: "pending",
+      },
+    ]);
+    setCart({});
+    Navigate("/orders");
+  };
+  useEffect(() => {
+    setOrderValue(
+      products.reduce((total, value) => {
+        return total + value.price * (cart[value.id] ?? 0);
+      }, 0)
+    );
+  }, [cart]);
   return (
     <div>
+      <h3>My Cart</h3>
+
+      {/* {Object.keys(cart).length > 0 ? (<h3>Cart</h3>) : <h3>Cart is empty</h3>} */}
+
       {products.map(
         (value) =>
           cart[value.id] > 0 && (
@@ -28,7 +58,15 @@ export default function Cart() {
           )
       )}
       <hr></hr>
-      <h3>Order Value:</h3>
+      <h4>Order Value:{orderValue}</h4>
+      <p>
+        {user.email ? (
+          <button onClick={placeOrder}>Place Order</button>
+        ) : (
+          <button onClick={()=>Navigate("/login")}>Login to Order</button>
+        )}
+      </p>
+      {/* <h4>Your Cart is empty</h4> */}
     </div>
   );
 }
